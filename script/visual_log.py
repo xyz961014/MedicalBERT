c306052133cb5250520424121987e8363aff22f7
xyz <xyz961014@gmail.com>
2021-12-19 01:46:54 +0800

    visual log

--- script/visual_log.py
+++ script/visual_log.py
@@ -11,7 +11,7 @@ def parse_args():
 
     parser.add_argument("--log_file", type=str, required=True,
                         help="dllogger json file")
-    parser.add_argument("--field", type=str, required=True,
+    parser.add_argument("--fields", type=str, required=True, nargs="+",
                         help="field name to visualize")
     parser.add_argument("--title", type=str, default=None,
                         help="picture title")
@@ -31,39 +31,42 @@ if __name__ == "__main__":
                             json_datum["data"]["valid_"+key] = json_datum["data"].pop(key)
             data.append(json_datum)
 
-    data_to_visualize = []
-    for datum in data:
-        for data_key in datum["data"].keys():
-            if args.field == data_key:
-                step = datum["step"][1]
-                value = datum["data"][data_key]
-                data_to_visualize.append({"step": step, "value": value})
-
-    # prepare data
-    x_name = "Step"
-    y_name = args.field
-    x_labels = np.array([item["step"] for item in data_to_visualize])
-    y_labels = np.array([item["value"] for item in data_to_visualize])
 
     fig, ax = plt.subplots(figsize=(20, 10))
-    
+
     # title
-    title = (args.field) if args.title is None else args.title
+    title = (" VS ".join(args.fields)) if args.title is None else args.title
     plt.title(title, fontsize=30)
 
-    # draw
+    # prepare data
+    plots = []
+    for field in args.fields:
+        data_to_visualize = []
+        for datum in data:
+            for data_key in datum["data"].keys():
+                if field == data_key:
+                    step = datum["step"][1]
+                    value = datum["data"][data_key]
+                    data_to_visualize.append({"step": step, "value": value})
+        x_name = "Step"
+        y_name = field
+        x_labels = np.array([item["step"] for item in data_to_visualize])
+        y_labels = np.array([item["value"] for item in data_to_visualize])
 
-    plt.grid(axis='y' ,color='grey', linestyle='--', lw=0.5, alpha=0.5)
-    plt.tick_params(axis='both', labelsize=24)
+        # draw
 
-    plot = ax.plot(x_labels, y_labels, 'blue', markersize=10, label=y_name, linewidth=2)
+        plt.grid(axis='y' ,color='grey', linestyle='--', lw=0.5, alpha=0.5)
+        plt.tick_params(axis='both', labelsize=24)
 
-    ax.set_xlabel(x_name, fontsize=24)
-    ax.set_ylabel(y_name, fontsize=24)
+        plot = ax.plot(x_labels, y_labels, markersize=10, label=y_name, linewidth=2)
+        plots.extend(plot)
 
-    ax.legend(plot, [l.get_label() for l in plot], fontsize=24)
+        ax.set_xlabel(x_name, fontsize=24)
+        ax.set_ylabel(y_name, fontsize=24)
+
+    ax.legend(plots, [l.get_label() for l in plots], fontsize=20)
 
     plt.tight_layout()
-    plt.savefig("{}.pdf".format(args.field))
+    plt.savefig("{}.pdf".format("_".join(args.fields)))
     
     plt.show()
